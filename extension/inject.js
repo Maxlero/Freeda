d19f6bc5bdb081f5f646badba5cc201e90a5d680
Max Arol <leroomax@gmail.com>
2024-05-13 16:45:10 +0200

    url fix?

--- extension/inject.js
+++ extension/inject.js
@@ -2,7 +2,7 @@
 const CACHE_URL_BASE = 'https://webcache.googleusercontent.com/search?q=cache:'
 
 console.log('injected :)')
-const currentUrl = window.location.href
+let currentUrl = window.location.href
 
 if (currentUrl) {
     console.log('Adding button on ', currentUrl)
@@ -80,63 +80,63 @@ const initOnCache = currentUrl => {
 
 // Starting point.
 // Waiting until document is loaded
-document.addEventListener('readystatechange', async event => {
-    if (event.target.readyState === 'complete') {
-        const currentUrl = window.location.href
-
-        // initOnMedium
-        if (currentUrl.startsWith('https://medium.com')) {
-
-            // TODO add coffee break notification
-            window.onscroll = function (ev) {
-                if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) {
-                    // you're at the bottom of the page
-                    console.log('Finally')
-                }
-            }
-
-            // console.log('loading complete')
-            // initOnMedium()
-            //     .then(upgradeButton => {
-            //         if (upgradeButton) {
-            //             console.log('success!')
-            //
-            //             // beautifying Upgrade button
-            //             upgradeButton.classList.add('upgrade_button')
-            //
-            //             // redirecting to cache page onClick
-            //             upgradeButton.onclick = () => {
-            //                 window.open(CACHE_URL_BASE + currentUrl, '_self')
-            //                 return false
-            //             }
-            //         } else {
-            //             console.log('Upgrade button not found, try reload the page')
-            //         }
-            //     })
-            //     .catch(err => console.log('Smth went wrong..', err))
-        }
-
-        // initOnCache
-        if (currentUrl.startsWith('https://webcache.googleusercontent.com/')) {
-
-            // TODO redirect back?
-            // window.history.replaceState(null,null, window.location.pathname + "/page2.php")
-
-            // TODO do smth when the page is not cached yet
-
-            // document.body.style = 'display:flex; flex-direction:column; justify-content:center;'
-            //
-            // const loadingSpinner = document.createElement('span')
-            // loadingSpinner.className = 'loader'
-            // document.body.insertBefore(loadingSpinner, document.body.firstChild)
-            //
-            // // hiding Google's bar
-            // const cacheBar = document.querySelectorAll('[id$=__google-cache-hdr]')
-            // if (cacheBar.length > 0) {
-            //     cacheBar[0].style.display = 'none'
-            // }
-            //
-            // initOnCache(currentUrl)
-        }
-    }
-})
+// document.addEventListener('readystatechange', async event => {
+//     if (event.target.readyState === 'complete') {
+//         const currentUrl = window.location.href
+//
+//         // initOnMedium
+//         if (currentUrl.startsWith('https://medium.com')) {
+//
+//             // TODO add coffee break notification
+//             window.onscroll = function (ev) {
+//                 if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) {
+//                     // you're at the bottom of the page
+//                     console.log('Finally')
+//                 }
+//             }
+//
+//             // console.log('loading complete')
+//             // initOnMedium()
+//             //     .then(upgradeButton => {
+//             //         if (upgradeButton) {
+//             //             console.log('success!')
+//             //
+//             //             // beautifying Upgrade button
+//             //             upgradeButton.classList.add('upgrade_button')
+//             //
+//             //             // redirecting to cache page onClick
+//             //             upgradeButton.onclick = () => {
+//             //                 window.open(CACHE_URL_BASE + currentUrl, '_self')
+//             //                 return false
+//             //             }
+//             //         } else {
+//             //             console.log('Upgrade button not found, try reload the page')
+//             //         }
+//             //     })
+//             //     .catch(err => console.log('Smth went wrong..', err))
+//         }
+//
+//         // initOnCache
+//         if (currentUrl.startsWith('https://webcache.googleusercontent.com/')) {
+//
+//             // TODO redirect back?
+//             // window.history.replaceState(null,null, window.location.pathname + "/page2.php")
+//
+//             // TODO do smth when the page is not cached yet
+//
+//             // document.body.style = 'display:flex; flex-direction:column; justify-content:center;'
+//             //
+//             // const loadingSpinner = document.createElement('span')
+//             // loadingSpinner.className = 'loader'
+//             // document.body.insertBefore(loadingSpinner, document.body.firstChild)
+//             //
+//             // // hiding Google's bar
+//             // const cacheBar = document.querySelectorAll('[id$=__google-cache-hdr]')
+//             // if (cacheBar.length > 0) {
+//             //     cacheBar[0].style.display = 'none'
+//             // }
+//             //
+//             // initOnCache(currentUrl)
+//         }
+//     }
+// })
